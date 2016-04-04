2f8696469c16e51ab9b93ab1a4bcbd2a05f4edfa
rodenr <rodenr@github.com>
2016-04-04 14:33:12 -0400

    starting graphics

---
+++ tkintest.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+from tkinter import *
+root = Tk()
+root.geometry("200x50-40+20")
+Label(root, text="Hello, world!", fg="red", font=("Helvetica", 30)).pack()
+root.mainloop()
